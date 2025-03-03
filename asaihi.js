ef8fe16a2aaa7c939545b6c7c865cf8e758e32d7
hamza-Amjad5163 <205333@aack.au.edu.pk>
2025-03-03 11:50:15 +0500

    asai in js
    Co-authored-by: hamza-Amjad5163 <205333@aack.au.edu.pk>
    Co-authored-by: hamzaAmjad5163 <hamzakhan5163@gmail.com>

---
+++ asaihi.js
@@ -0,0 +1,41 @@
+jhgj 
+knkl 
+
+// app.js   
+var asaihi = require('./asaihi.js');
+
+console.log(asaihi.sayHello()); // Outputs: Hello, asaihi!
+
+// asaihi.js
+
+module.exports = {
+  sayHello: function() {
+    return 'Hello, asaihi!';
+  }
+};
+
+// main.js
+var asaihi = require('./asaihi.js');
+
+console.log(asaihi.sayHello()); // Outputs: Hello, asaihi!
+
+// asaihi.js
+module.exports = {
+  sayHello: function() {
+    return 'Hello, asaihi!';
+  }
+};
+
+// main.js
+
+var asaihi = require('./asaihi.js');
+
+console.log(asaihi.sayHello()); // Outputs: Hello, asaihi!
+
+// asaihi.js
+
+module.exports = {
+  sayHello: function() {
+    return 'Hello, asaihi!';
+  }
+};
